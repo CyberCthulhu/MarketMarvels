9f2e8e66cbaab019c6e9ce653f53138222604611
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2024-06-10 16:58:46 -0400

    template completed

--- backend/src/main/java/com/example/MarketMarvels/MarketMarvelsApplication.java
+++ backend/src/main/java/com/example/MarketMarvels/MarketMarvelsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketMarvelsApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(MarketMarvelsApplication.class, args);
 	}
 

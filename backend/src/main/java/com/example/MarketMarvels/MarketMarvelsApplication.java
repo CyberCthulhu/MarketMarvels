4900fc4f266643eddc865fe3c1c92baf6a50ee3b
Chris Morales Barcelo <csmorales2002@gmail.com>
2024-06-10 13:39:42 -0500

    backend information

---
+++ backend/src/main/java/com/example/MarketMarvels/MarketMarvelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarketMarvels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketMarvelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketMarvelsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/MarketMarvels/MarketMarvelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarketMarvels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketMarvelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

80777c3abf76c0488784fe252fc0bb44e1be183a
alishamorejon <alishasmorejon@gmail.com>
2024-06-14 10:22:11 -0500

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,20 @@
-fetch('http://localhost:8000/api/users')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(user => {
-            console.log(user);
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+// fetch('http://localhost:8000/api/users')
+//     .then(response => response.json())
+//     .then(data => {
+//         data.forEach(user => {
+//             console.log(user);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+
+function showDesription(serviceID){
+    // hide all descriptions
+    var descriptions = document.getElementsByClassName('description');
+    for (var i=0; i<descriptions.length; i++){
+        descriptions[i].style.display = 'none';
+    }
+    // show the selected description
+    document.getElementById(serviceID).style.display = 'block';
+}
